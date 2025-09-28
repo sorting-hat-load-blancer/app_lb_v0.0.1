7c7058c4ba3c40f0a5a106b3c1c3d86009b8ce35
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-28 09:03:14 +0530

    send to respon client

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,25 @@ pub fn handle_client(mut tcp_stream: TcpStream){
         "fail to read client data"
     );
     
+    // convert buffer data to string
+    let data = String::from_utf8_lossy(&buffer[..]);  //decode byte -> string
+    
+    println!("Recived request : {}", data);
+    
+    let response = "HTTP/1.1 200 OK\r\n\
+                         Content-Type: text/plain\r\n\r\n\
+                         Hii hello client i am sorting-hat-load-balancer".as_bytes();
+    
+    tcp_stream.write(response).expect(
+        "fail send res data to client"
+    );
+    
 }
 
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").expect(
+        "fail to bind address 127.0.0.1:8080"
+    );
+    
+    println!("");
 }

bb217676c62e96476098ec3407b7e9f421eb29fb
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-28 09:25:35 +0530

    fix warning msg

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::net::{TcpStream,TcpListener};
 fn handle_client(mut tcp_stream: TcpStream){
     let mut buffer = [0;4096];
     
-    tcp_stream.read((&mut buffer)).expect(
+    tcp_stream.read(&mut buffer).expect(
         "fail to read client data"
     );
     

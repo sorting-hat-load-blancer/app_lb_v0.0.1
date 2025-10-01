6f49acd2afece5e5c797ed4e28370615f409e896
thushan-123 <thushanmadusanka456@gmail.com>
2025-10-01 10:36:22 +0530

    read data fron dest

--- src/load_balancer_algo/forward.rs
+++ src/load_balancer_algo/forward.rs
@@ -16,6 +16,19 @@ pub fn forward_request(tcp_stream: TcpStream, send_payload: &str){
             "fail to send data to destination server"
         );
 
+    let mut buffer = [0;1024];
+
+    match stream.read(&mut buffer){
+        Ok(bytes_read) => {
+            let response = String::from_utf16_lossy(&buffer[..bytes_read]);
+            println!("Received response {}", response);
+        }
+        Err(e) => {
+            eprintln!("fail to read data from server {}",e);
+        }
+    }
+
+    Ok(());
 
 
 
